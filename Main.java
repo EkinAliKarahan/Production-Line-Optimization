e944daeb300953e269419cfba19f58f9f4b0ee0a
Ekin Ali Karahan <146540909+EkinAliKarahan@users.noreply.github.com>
2025-04-29 18:01:06 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,7 +9,8 @@ class UretimHattiOptimizasyonu {
         int[][] isSüreleri = {
                 {7, 40},
                 {6, 5},
-                {8, 2}
+                {8, 2},
+                {12,3}
         };
 
         int[][] gecisMaliyetleri = {
@@ -25,27 +26,25 @@ class UretimHattiOptimizasyonu {
         int isSayisi = isSüreleri.length;
         int makineSayisi = isSüreleri[0].length;
 
-        // DP tablosu: dp[i][j] = i. işi j. makinede bitirdiğimizdeki minimum süre
+
         int[][] dp = new int[isSayisi][makineSayisi];
 
-        // İlk iş için başlangıç süreleri (geçiş maliyeti yok)
+
         for (int makine = 0; makine < makineSayisi; makine++) {
             dp[0][makine] = isSüreleri[0][makine];
         }
-        //dp[i][j]: i. işi j. makinede bitirdiğimizdeki minimum süreyi tutar.
-        //dp[i][j] hesaplanarak tüm işlerin ve makinelerin en iyi sıralaması yapılır.
 
 
-        // Sonraki işler için DP
+        
         for (int is = 1; is < isSayisi; is++) {
             for (int mevcutMakine = 0; mevcutMakine < makineSayisi; mevcutMakine++) {
                 int minSüre = Integer.MAX_VALUE;
 
-                // Önceki işin hangi makinede bittiğine bak
+
                 for (int oncekiMakine = 0; oncekiMakine < makineSayisi; oncekiMakine++) {
                     int süre = dp[is - 1][oncekiMakine]  //Önceki işin hangi makinede bitirildiği bilgisi.
                             + gecisMaliyetleri[oncekiMakine][mevcutMakine] //Makine değişimi için geçiş maliyeti.
-                            + isSüreleri[is][mevcutMakine]; //Bu işin şu anki makinede süresi.
+                            + isSüreleri[is][mevcutMakine]; //İşin güncel makinedeki süresi.
                     if (süre < minSüre) {
                         minSüre = süre;
                     }
@@ -55,7 +54,7 @@ class UretimHattiOptimizasyonu {
             }
         }
 
-        // Son işin minimum süresini bul
-        return Arrays.stream(dp[isSayisi - 1]).min().orElseThrow();   //En küçük süreyi bulur.
+
+        return Arrays.stream(dp[isSayisi - 1]).min().orElseThrow();
     }
 }
